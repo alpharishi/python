1a6ebc5f7396b87a45972e3b97732e8076939f1f
Rishi <alpharishi@outlook.com>
2020-07-24 15:57:52 +0100

    Updated with some more stuff

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,28 @@
-#This is sample commit
+# This is sample commit
 print("Govinda Govinda......")
 print(" How old are you?")
 print('rishi "blah blah"')
-print (1 * 'rishi')
+print('\nrishi')
 print(r'\navin')
+print(2+3)
+x=2
+print(x+3)
+print(x+150)
+
+name= 'youtube'
+print(name)
+print(name+' rocks')
+print(name[5])
+print(name[1:8])
+mucakue= 'rISHI'
+print(len(mucakue))
+nums = [25,12,36,95,14]
+names = ['Rishi','Rayan','Bum']
+print(nums)
+print(nums[0:3])
+print(nums[-5])
+print(names)
+print(names[1])
+
+both = [nums, names]
+print(both)
